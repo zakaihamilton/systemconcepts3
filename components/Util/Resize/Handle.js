8ae0b9d3d4601f3f46307aaf4cbb9b5fd2978669
Zakai Hamilton <zakaihamilton@gmail.com>
2022-06-12 22:53:59 +0300

    fixed resize component

--- components/Util/Resize/Handle.js
+++ components/Util/Resize/Handle.js
@@ -3,19 +3,21 @@ import Resize from "../Resize";
 import { useEventListener } from "../EventListener";
 
 export default function Handle({ handle, enabled = true, children = null }) {
-    const resize = useRef();
+    const size = useRef();
     const resizeState = Resize.State.useState();
     const globalDocument = typeof document !== "undefined" && document;
     const moveTarget = useCallback(e => {
         const target = resizeState?.target;
-        const [left, top] = resizeState?.offset;
+        const [width, height] = resizeState?.offset;
         if (!target) {
             return;
         }
-        const targetRegion = target.getBoundingClientRect();
-        const targetWidth = e.clientX - targetRegion.left + left;
-        const targetHeight = e.clientY - targetRegion.top + top;
-        resize.current = [targetWidth, targetHeight];
+        const targetWidth = e.clientX - width;
+        const targetHeight = e.clientY - height;
+        size.current = [targetWidth, targetHeight];
+        if (typeof resizeState.handler === "function") {
+            [targetWidth, targetHeight] = resizeState.handler(targetWidth, targetHeight);
+        }
         target.style.width = targetWidth + 'px';
         target.style.height = targetHeight + 'px';
     }, [resizeState]);
@@ -25,12 +27,13 @@ export default function Handle({ handle, enabled = true, children = null }) {
         }
         resizeState.resizing = true;
         const handleRegion = e.target.getBoundingClientRect();
-        resizeState.offset = [e.clientX - handleRegion.left + 1, e.clientY - handleRegion.top + 1];
+        const targetRegion = resizeState?.target.getBoundingClientRect();
+        resizeState.offset = [e.clientX - targetRegion.width + 1, e.clientY - targetRegion.height + 1];
         moveTarget(e);
     }, [resizeState, moveTarget]);
     useEventListener(resizeState?.resizing && globalDocument, "mousemove", moveTarget, [resizeState, moveTarget]);
     useEventListener(resizeState?.resizing && globalDocument, "mouseup", e => {
-        Object.assign(resizeState, { width: resize.current[0], height: resize.current[1], resizing: false });
+        Object.assign(resizeState, { width: size.current[0], height: size.current[1], resizing: false });
         moveTarget(e);
     }, [resizeState, moveTarget]);
     return children;
